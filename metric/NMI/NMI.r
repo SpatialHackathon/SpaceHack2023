0aa86250eb3abaa8f8286932d7dae3d87904a9f2
Mark Robinson <mark.robinson@mls.uzh.ch>
2023-12-13 13:03:57 +0100

    add correct implementation

--- metric/NMI/NMI.r
+++ metric/NMI/NMI.r
@@ -60,14 +60,16 @@ if (!is.na(opt$config)) {
 
 ## Code for calculating metric goes here
 ## --------------------------------------
-library(clue)
-library(jsonlite)
+## Code for calculating metric goes here
+## --------------------------------------
 
-# # for testing - start
+library(aricode)
+
+# for testing - start
 # label_file <- "results/libd_dlpfc/Br5595_151670/SpaGCN/domains.tsv"
 # groundtruth_file <- "data/libd_dlpfc/Br5595_151670/labels.tsv"
-# outfile <- "./domain-specific-f1.json"
-# # for testing - stop
+# outfile <- "./NMI.txt"
+# for testing - stop
 
 domains <- read.delim(label_file, sep="\t", row.names = 1)
 groundtruth <- read.delim(groundtruth_file, sep="\t", row.names = 1)
@@ -78,53 +80,13 @@ rn <- intersect(rownames(domains), rownames(groundtruth))
 domains <- domains[rn,,drop = FALSE]
 groundtruth <- groundtruth[rn,,drop = FALSE]
 
-n_gt_labels <- length(unique(groundtruth$label))
-n_clust_labels <- length(unique(domains$label))
+metric <- NMI(domains$label,groundtruth$label)
 
-
-tb <- table(domains$label, groundtruth$label)
-
-# hungarian solver will fail, pad with 0 columns
-if( n_clust_labels > n_gt_labels ) {
-  n_cols <- n_clust_labels - n_gt_labels
-  for(i in seq_len(n_cols)) {
-    tb <- cbind(tb,0)
-    colnames(tb)[ncol(tb)] <- paste0("dummy",i)
-  }
-}
-
-print(tb)
-
-# for the case that there are more clusters than true labels, need to 
-# add dummy variables
-
-hg <- clue::solve_LSAP(tb, maximum = TRUE)
-sa <- seq_along(hg)
-mapping <- data.frame(cluster = rownames(tb)[sa], 
-                      label = colnames(tb)[hg],
-                      tp = tb[cbind(seq_along(hg), hg)])
-rownames(mapping) <- mapping$label
-
-tt <- table(groundtruth$label)
-mapping[names(tt),"domain_size"] <- tt
-
-tl <- table(domains$label)
-m <- match(mapping$cluster, names(tl))
-mapping$cluster_size <- as.numeric(tl[m])
-
-mapping$recall <- with(mapping, tp/domain_size)
-mapping$precision <- with(mapping, tp/cluster_size)
-mapping$f1 <- with(mapping, 2/(1/recall+1/precision))
-
-# remove dummy labels
-(mapping <- mapping[!grepl("dummy", mapping$label),])
+print(metric)
 
 ## Write output
 outfile <- file(opt$out_file)
-write_json(unname(mapping[,c("label","f1")]),outfile)
 
-# from template
-# dir.create(dirname(outfile), showWarnings = FALSE, recursive = TRUE)
-# 
-# writeLines(format(metric, digits = 6, scientific = TRUE), outfile)
-# close(outfile)
+dir.create(dirname(outfile), showWarnings = FALSE, recursive = TRUE)
+writeLines(format(metric, digits = 6, scientific = TRUE), outfile)
+close(outfile)
