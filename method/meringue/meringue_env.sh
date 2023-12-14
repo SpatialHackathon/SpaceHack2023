e35bc7aba624a0b1601832f72b30a9bac7b9889d
shdam <sohdam@dtu.dk>
2023-12-14 11:14:54 +0000

    add git commit tag

--- method/meringue/meringue_env.sh
+++ method/meringue/meringue_env.sh
@@ -7,6 +7,6 @@ conda env create -f meringue.yml
 source activate meringue_env
 
 # Install the required R packages
-Rscript -e "remotes::install_github('JEFworks-Lab/MERINGUE')"
+Rscript -e "remotes::install_github('JEFworks-Lab/MERINGUE', ref = 'ca9e2ccabd95680d9ca0b323a8a507c038f2ea13')"
 
 
