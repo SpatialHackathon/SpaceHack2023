f77587d193e76f9a0b19b986d032baaa39db66d1
gmoranzoni <giorgia.moranzoni@gmail.com>
2024-01-22 09:07:26 +0000

    updated yml file with correct seurat version

--- method/maple/maple_env.sh
+++ method/maple/maple_env.sh
@@ -7,4 +7,5 @@ conda env create -f maple.yml
 source activate maple_env
 
 # Install the required R packages
+Rscript -e "remotes::install_github('carter-allen/spruce', ref = '47b02300cc9a1d83213682bd78464115867d1763')"
 Rscript -e "remotes::install_github('carter-allen/maple', ref = 'b173e89a7bc82c6ae09c7e0709d09ed22082172d')"
