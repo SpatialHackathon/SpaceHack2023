9bb480ce3418d4cb409faa75e0d08e4d63bbecbc
Jieran Sun <jiersun99@gmail.com>
2024-02-02 09:46:15 +0000

    Update environment shell script

--- method/SC.MEB/scmeb_env.sh
+++ method/SC.MEB/scmeb_env.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Create the SC.MEB conda environment named scmeb_env
-conda env create -f SC.MEB.yml
+# conda env create -f SC.MEB.yml
 
 # Activate the environment
-source activate scmeb_env
+# source activate scmeb_env
 
 # Install the required R packages
 conda run -n scmeb_env R -e "install.packages('SC.MEB')"
