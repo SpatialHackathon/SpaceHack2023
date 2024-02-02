90630b153f0ae3503b2aee5a155eaf8254ac479f
Jieran Sun <jiersun99@gmail.com>
2024-02-02 10:28:30 +0000

    update scmeb

--- method/SC.MEB/scmeb_env.sh
+++ method/SC.MEB/scmeb_env.sh
@@ -8,5 +8,5 @@
 
 # Install the required R packages
 # conda run -n scmeb_env R -e "install.packages('SC.MEB')"
-Rscript -e "install.packages('SC.MEB')"
+Rscript -e "remotes::install_version(package = 'SC.MEB', version = '1.1', repos = 'https://cran.uni-muenster.de/')"
 
