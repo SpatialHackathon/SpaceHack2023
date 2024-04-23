1cda3ac190e86579237e2d2ddadaaaccdae9b046
shdam <sohdam@dtu.dk>
2024-04-23 12:28:09 +0000

    Native Meringue

--- method/meringue/meringue.r
+++ method/meringue/meringue.r
@@ -56,6 +56,11 @@ option_list <- list(
     help = "Number of clusters to return."
   ),
   make_option(
+    c("--n_pcs"),
+    type = "integer", default = NULL,
+    help = "Number of PCs to use."
+  ),
+  make_option(
     c("--technology"),
     type = "character", default = NULL,
     help = "The technology of the dataset (Visium, ST, imaging-based)."
@@ -90,7 +95,6 @@ embedding_file <- file.path(out_dir, "embedding.tsv")
 # Use these filepaths as input ...
 if (!is.na(opt$neighbors)) {
   neighbors_file <- opt$neighbors
-  neighbors <- as(Matrix::readMM(neighbors_file), "CsparseMatrix")
 }
 
 if (!is.na(opt$dim_red)) {
@@ -103,6 +107,9 @@ if (!is.na(opt$image)) {
 if (!is.na(opt$config)) {
   config_file <- opt$config
     config <- fromJSON(config_file)
+}
+if (!is.na(opt$matrix)) {
+  matrix_file <- opt$matrix
 }
 
 
@@ -122,73 +129,63 @@ if ("selected" %in% colnames(positions)) {
         positions <- positions[as.logical(positions$selected), c(1, 2)]
   }
 
+# Load configuration
+k <- config$k
+alpha <- config$alpha
+beta <- config$beta
+n_pcs <- config$n_pcs
+n_pcs <- ifelse(is.null(opt$n_pcs), n_pcs, opt$n_pcs)
+filterDist <- config$filterDist
+
 # Seed
 seed <- opt$seed
 set.seed(seed)
 
-## Your code goes here
-k <- config$k
-alpha <- config$alpha
-beta <- config$beta
+# Preprocessing
+counts <- Matrix::t(Matrix::readMM(matrix_file))
+colnames(counts) <- rownames(positions)
+counts <- cleanCounts(counts = counts, 
+                      min.reads = 100, 
+                      min.lib.size = 100, 
+                      plot=FALSE,
+                      verbose=TRUE)
 
-# Resolution optimization
-binary_search <- function(
-    dimred,
-    do_clustering,
-    n_clust_target,
-    resolution_boundaries,
-    num_rs = 100,
-    tolerance = 1e-3,
-    ...) {
+positions <- positions[colnames(counts),]
 
-  # Initialize boundaries
-  lb <- rb <- NULL
-  n_clust <- -1
+counts <- normalizeCounts(counts, log=FALSE, verbose=TRUE)
 
-  lb <- resolution_boundaries[1]
-  rb <- resolution_boundaries[2]
+# PCA
+pcs.info <- prcomp(t(log10(as.matrix(counts)+1)), center=TRUE)
+pcs <- pcs.info$x[,1:n_pcs]
 
-  i <- 0
-  while ((rb - lb > tolerance || lb == rb) && i < num_rs) {
-    mid <- sqrt(lb * rb)
-    message("Resolution: ", mid)
-    new_louvain <- igraph::cluster_louvain
-    formals(new_louvain)$resolution <- mid
-    cluster_ids <- do_clustering(dimred, method = new_louvain, ...)
-    # Adjust cluster_ids extraction per method
-    n_clust <- length(unique(cluster_ids))
-    message("Cluster: ", n_clust)
-    if (n_clust == n_clust_target || lb == rb) break
-    if (n_clust > n_clust_target) {
-      rb <- mid
-    } else {
-      lb <- mid
-    }
-    i <- i + 1
-  }
-
-  # Warning if target not met
-  if (n_clust != n_clust_target) {
-    warning(sprintf("Warning: n_clust = %d not found in binary search, return best approximation with res = %f and n_clust = %d. (rb = %f, lb = %f, i = %d)", n_clust_target, mid, n_clust, rb, lb, i))
-  }
-  return(cluster_ids)
-}
 
 # Spatial neighbors
-W <- getSpatialNeighbors(positions, filterDist = 2)
+W <- getSpatialNeighbors(positions, filterDist = filterDist)
 
 # Clustering
-result <- binary_search(dimred, n_clust_target = n_clusters, resolution_boundaries = c(0.1, 2), 
-                        do_clustering = getSpatiallyInformedClusters, 
-                        # Meringue specific
-                        W = W, 
-                        k = k,
-                        alpha = alpha,
-                        beta = beta)
+source("/home/jovyan/native/method/search_res.r")
+extract_nclust <- function(result){
+    length(unique(result))
+}
+do_clustering <- function(pcs, resolution, ...){
+    new_louvain <- igraph::cluster_louvain
+    formals(new_louvain)$resolution <- resolution
+    getSpatiallyInformedClusters(pcs, method = new_louvain, ...)
+}
 
-#labels <- getSpatiallyInformedClusters(dimred, W = neighbors, k = k, alpha = alpha, beta = beta)
-label_df <- data.frame("label" = result, row.names = rownames(dimred))
+result <- binary_search(
+    pcs, 
+    n_clust_target = n_clusters, 
+    extract_nclust = extract_nclust,
+    do_clustering = do_clustering, 
+    # Meringue specific
+    W = W, 
+    k = k,
+    alpha = alpha,
+    beta = beta)
 
+
+label_df <- data.frame("label" = result, row.names = rownames(pcs))
 
 ## Write output
 dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
